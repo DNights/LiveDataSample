91e345e5050d5f76c258d5268bff11240ab7911b
hyundonghan <guehgk456@gmail.com>
2019-09-03 16:18:44 +0900

    sample test

--- app/src/main/java/com/dnights/livedatasample/MainActivity.kt
+++ app/src/main/java/com/dnights/livedatasample/MainActivity.kt
@@ -2,11 +2,27 @@ package com.dnights.livedatasample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
 
 class MainActivity : AppCompatActivity() {
+
+    val temp: MutableLiveData<String> = MutableLiveData()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        temp.observe(this, Observer {
+            println("$it")
+        })
+
+        funtion1()
+    }
+
+    fun funtion1 () {
+        temp.value = "1234"
+        temp.postValue("5678")
     }
 }
