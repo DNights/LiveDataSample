6b0542ab9c79e67d93453914dd0fd17fe15c192a
hyun dong han <guehgk456@gmail.com>
2019-10-01 22:49:56 +0900

    fix code setting databinding in nameActivity

--- app/src/main/java/com/dnights/livedatasample/view/NameActivity.kt
+++ app/src/main/java/com/dnights/livedatasample/view/NameActivity.kt
@@ -2,36 +2,28 @@ package com.dnights.livedatasample.view
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.dnights.livedatasample.R
+import com.dnights.livedatasample.databinding.ActivityNameBinding
 import com.dnights.livedatasample.viewmodel.NameViewModel
 import kotlinx.android.synthetic.main.activity_name.*
 
 class NameActivity : AppCompatActivity() {
 
     private lateinit var model: NameViewModel
+    private lateinit var binding: ActivityNameBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_name)
-
-        // Other code to setup the activity...
-
-        // Get the ViewModel.
         model = ViewModelProviders.of(this).get(NameViewModel::class.java)
 
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_name)
+        binding.lifecycleOwner = this
+        binding.vm = model
 
-        // Create the observer which updates the UI.
-        val nameObserver = Observer<String> { newName ->
-            // Update the UI, in this case, a TextView.
-            nameTextView.text = newName
-        }
-
-        // Observe the LiveData, passing in this activity as the LifecycleOwner and the observer.
-        model.currentName.observe(this, nameObserver)
-
-        button.setOnClickListener {
+        binding.button.setOnClickListener {
             val anotherName = "John Doe"
             model.currentName.setValue(anotherName)
         }
