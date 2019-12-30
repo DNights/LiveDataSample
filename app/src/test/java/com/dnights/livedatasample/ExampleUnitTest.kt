79398a625bae2fcf8e874840ea0584ca4f55d1be
hyun dong han <guehgk456@gmail.com>
2019-12-30 10:49:23 +0900

    add test code

--- app/src/test/java/com/dnights/livedatasample/ExampleUnitTest.kt
+++ app/src/test/java/com/dnights/livedatasample/ExampleUnitTest.kt
@@ -1,8 +1,11 @@
 package com.dnights.livedatasample
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.dnights.livedatasample.viewmodel.NameViewModel
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -10,8 +13,27 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+    @Rule
+    @JvmField
+    val rule = InstantTaskExecutorRule()
+
+    private lateinit var viewModel: NameViewModel
+
+    @Before
+    fun before(){
+        viewModel = NameViewModel()
+    }
+
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
+    }
+
+    @Test
+    fun nameTest(){
+        val anotherName = "John Doe"
+        viewModel.currentName.value = anotherName
+
+        assertEquals(viewModel.currentName.value , anotherName)
     }
 }
